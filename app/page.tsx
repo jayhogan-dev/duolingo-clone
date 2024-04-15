4f19f89a99c516cf1236524f6c1d6fe52db87733
Jay Hogan <jayhogan.dev@gmail.com>
2024-04-15 13:54:27 -0400

    button component library created

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button
-        size="lg"
-        variant="destructive"
-      >
-        Click
-      </Button>
+      <p>Home</p>
     </div>
   );
 }
