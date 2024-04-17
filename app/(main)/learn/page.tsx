78afb5bd36739acf28d879e02597f06bdf0d7679
Jay Hogan <jayhogan.dev@gmail.com>
2024-04-17 11:31:09 -0400

    added getUnits query - need to updated later for performance reasons

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -4,12 +4,16 @@ import FeedWrapper from "@/components/FeedWrapper";
 import StickyWrapper from "@/components/StickyWrapper";
 import Header from "./header";
 import UserProgress from "@/components/UserProgress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -27,6 +31,14 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div
+            key={unit.id}
+            className="mb-10"
+          >
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
