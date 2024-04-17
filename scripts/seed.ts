3a1b80e5a6687acbcf4d67a43c1259affcf76c22
Jay Hogan <jayhogan.dev@gmail.com>
2024-04-17 10:49:37 -0400

    seed file updated to included challenges, chall options, units, and lessons

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -40,6 +45,62 @@ const main = async () => {
         id: 5,
         title: "Italian",
         imageSrc: "/it.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 3,
+        title: "Unit 1",
+        description: "Learn the basics",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Getting Started",
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: "Which one of these is 'man'?",
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "man",
+        audioSrc: "/swe_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "kvinna",
+        audioSrc: "/swe_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/child.svg",
+        correct: false,
+        text: "barn",
+        audioSrc: "/swe_child.mp3",
       },
     ]);
 
